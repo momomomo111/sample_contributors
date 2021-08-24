5058e34eafcd6872dc87d5d5791fe1aba5cf7759
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-08-24 22:49:35 +0900

    [update] Introducing ktlintFormat
    ktlintFormatの導入

--- app/src/main/java/com/moasanuma/sample_contributors/MainActivity.kt
+++ app/src/main/java/com/moasanuma/sample_contributors/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.moasanuma.sample_contributors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.moasanuma.sample_contributors.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/moasanuma/sample_contributors/ui/main/MainFragment.kt
+++ app/src/main/java/com/moasanuma/sample_contributors/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.moasanuma.sample_contributors.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.moasanuma.sample_contributors.R
 
 class MainFragment : Fragment() {
@@ -17,7 +17,8 @@ class MainFragment : Fragment() {
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
@@ -28,5 +29,4 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
