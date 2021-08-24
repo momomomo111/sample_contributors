5655a6ca3dfee224d2f756c1079530b42888ddf3
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-08-24 17:58:58 +0900

    [init] Initialization commit
    初期化コミット

---
+++ app/src/main/java/com/moasanuma/sample_contributors/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.moasanuma.sample_contributors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moasanuma.sample_contributors.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/moasanuma/sample_contributors/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.moasanuma.sample_contributors.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
