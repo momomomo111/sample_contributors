50b2faec746be2ec75aa39f12901a9c97446722a
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-08-30 15:57:52 +0900

    Revert "[add] Add layout and other features for detail screen display."
    This reverts commit 42bd188a8a3c8821cb82cdf94d4669daf523f9e2.

--- app/src/main/java/com/moasanuma/sample_contributors/detail/DetailViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.moasanuma.sample_contributors.detail
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import com.moasanuma.sample_contributors.network.ContributorsProperty
-
-class DetailViewModel(@Suppress("UNUSED_PARAMETER")contributorsProperty: ContributorsProperty, app: Application) : AndroidViewModel(app) {
-}
